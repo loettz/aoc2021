016461ff307d14225dd32ebf700c2413f3387602
Lotte Hansen <lotte_hansen@web.de>
2021-12-11 00:15:40 +0100

    day 3 part 2

--- day3/day3.js
+++ day3/day3.js
@@ -18,29 +18,50 @@ export const getColumn = (arr, index) => {
     return columnValues;
 }
 
-export const getZeroesinColumn = (column) => {
-    return column.filter((elem) => elem === 0)
+const countOccurences = (column) => {
+    return column.reduce( (acc, val) => {
+        acc[val] = (acc[val] || 0 ) + 1
+        return acc
+    },{})
 }
-
-export const getMostCommonBit = (column) => {
-    return getZeroesinColumn(column).length >= column.length / 2 ? 0: 1;
-}
-
-export const getLeastCommonBit = (column) => {
-    return getZeroesinColumn(column).length >= column.length / 2 ? 1: 0;
-}
-
 
 export const getConsumption = (input) => {
     let gammaRate = '';
     let epsilonRate = '';
     const rowLength = input[0].length
+
+
     for (let i = 0; i < rowLength; i++) {
-        gammaRate += getMostCommonBit(getColumn(input, i)).toString();
-        epsilonRate += getLeastCommonBit(getColumn(input, i)).toString();
+        const counter = countOccurences(getColumn(input,i));
+        const mostCommon =  Object.keys(counter).reduce((a, b) => counter[a] > counter[b] ? a : b)
+        const leastCommon =  Object.keys(counter).reduce((a, b) => counter[a] > counter[b] ? b : a)
+        gammaRate += mostCommon;
+        epsilonRate += leastCommon;
     }
 
-    return gammaRate * epsilonRate;
+
+    return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
 }
 
-console.log(getConsumption(separatedNumbers));
+const filterInput = (input, searchMostCommon) => {
+    let array = input;
+    for (let i = 0; i < input[0].length; i++) {
+        let col = getColumn(array, i);
+        const counter = countOccurences(col);
+
+        if (array.length > 1) {
+            const mostCommon =  Object.keys(counter).reduce((a, b) => counter[a] > counter[b] ? a : b)
+            const leastCommon =  Object.keys(counter).reduce((a, b) => counter[a] > counter[b] ? b : a)
+            const filter = searchMostCommon ? mostCommon: leastCommon;
+            array = array.filter((elem) => elem[i] === parseInt(filter));
+        }
+    }
+
+    return parseInt(array.flat().join(''), 2);
+}
+
+export const calculateLifeSupportRating = (input) => {
+    return filterInput(input, true) * filterInput(input, false);
+}
+
+console.log(calculateLifeSupportRating(separatedNumbers))
